f35daef1f81681d48df384d6c122c99b8c31749d
3ADaniel <00001107047201sp@al.educacao.sp.gov.br>
2024-11-27 16:45:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Desenho',
+    'Qual é heroi é o mais forte?',
+    'Hulk'
+)
+
+criaCartao(
+    'Desenho',
+    'Qual é heroi mais rapido? ',
+    'Flash'
+)
+
+criaCartao(
+    'Desenho',
+    'Qual é o heroi que mora na agua?',
+    'Aquaman'
+)
+
+criaCartao(
+    'Desenho',
+    'Qual é a heroina mais forte? ',
+    'Mulher-Maravilha'
+)
+
+criaCartao(
+    'Desenho',
+    'Qual é heroi usa martelo',
+    'Thor'
+)
+
+criaCartao(
+    'Desenho',
+    'Qual é heroi usa escudo?',
+    'Capitão-America'
+)
